5ec0bc2a1003992b3a71f0d37781d15198b2e206
thienpham-0812 <thienpham-0812@example.com>
2021-10-05 08:38:00 +0700

    update-dsign-home-page

--- resources/views/frontend/footer.blade.php
+++ resources/views/frontend/footer.blade.php
@@ -1,27 +1,104 @@
-<div class="row footer">
-    <div class="company col-md-3">
-        <h4>Company</h4>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Voluptate, autem sequi expedita dicta, nihil voluptatum suscipit nam quaerat
-        corporis iusto beatae harum reiciendis natus ab eum atque excepturi quae ea?
-    </div>
-    <div class="contact col-md-3">
-        <h4>Contact</h4>
-        <p>
-            Địa chỉ: 144 Xuân Thủy, Cầu Giấy, Hà Nội
-        </p>
-        <p>
-            Số điện thoại: (+84) 0948.253.888
-        </p>
-        <p>
-            Block-Chain: Vũ Hữu Hải Vũ (Trưởng nhóm)
-        </p>
-        <p>
-            BackEnd: Phạm Văn Thiện
-        </p>
-        <p>
-            FrontEnd: Kiều Văn Tùng
-        </p>
+<footer class="footer">
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-md-6 col-lg-4 ">
+                    <div class="footer_widget">
+                        <div class="footer_logo">
+                            <a href="#">
+                                <img src="{{ asset('images/footer_logo.png') }}" alt="">
+                            </a>
+                        </div>
+                        <p class="address_text">Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit, sed do
+                            <br> eiusmod tempor incididunt ut labore.
+                        </p>
+                        <div class="socail_links">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <i class="ti-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="ti-twitter-alt"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-dribbble"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
 
+                    </div>
+                </div>
+                <div class="col-xl-2 col-md-6 col-lg-2">
+                    <div class="footer_widget">
+                        <h3 class="footer_title">
+                            Services
+                        </h3>
+                        <ul class="links">
+                            <li><a href="#">Quyên góp</a></li>
+                            <li><a href="#">Tài trợ</a></li>
+                            <li><a href="#">Gây quỹ</a></li>
+                            <li><a href="#">Tình nguyện viên</a></li>
+                            <li><a href="#">Việc làm</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="footer_widget">
+                        <h3 class="footer_title">
+                            Contacts
+                        </h3>
+                        <div class="contacts">
+                            <p>+2(305) 587-3407 <br> info@loveuscharity.com <br> Flat 20, Reynolds Neck, North
+                                Helenaville, FV77 8WS
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="footer_widget">
+                        <h3 class="footer_title">
+                            Members
+                        </h3>
+                        <ul class="news_links">
+                            <li>
+                                Vũ Hữu Hải Vũ (Blockchain - Trưởng nhóm)
+                            </li>
+                            <li>
+                                Phạm Văn Thiện (Backend - Thành viên)
+                            </li>
+                            <li>
+                                Kiều Văn Tùng (FrontEnd - Thành viên)
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+    <div class="copy-right_text">
+        <div class="container">
+            <div class="row">
+                <div class="bordered_1px "></div>
+                <div class="col-xl-12">
+                    <p class="copy_right text-center">
+                    <p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Crate By: Team VTT
+                    </p>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
