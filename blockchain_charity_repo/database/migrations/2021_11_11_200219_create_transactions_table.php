7faa77ca56dff07f12daf1762a024097d2f2f997
VuHai-99 <vuhuuhaivu9900@gmail.com>
2021-11-26 14:29:47 +0700

    fix append transaction time in transaction db

--- blockchain_charity_repo/database/migrations/2021_11_11_200219_create_transactions_table.php
+++ blockchain_charity_repo/database/migrations/2021_11_11_200219_create_transactions_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateTransactionsTable extends Migration
@@ -13,13 +14,15 @@ class CreateTransactionsTable extends Migration
      */
     public function up()
     {
+        
         Schema::create('transactions', function (Blueprint $table) {
             $table->string('transaction_hash')->primary();
             $table->string('sender_address');
             $table->string('receiver_address');
             $table->integer('transaction_type');
             $table->string('amount');
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
